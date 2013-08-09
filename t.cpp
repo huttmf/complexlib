47d447c15fcd08f49181bfd92e090f30e7202604
Mike <mhutt@appternion.com>
2013-08-09 15:22:42 -0400

    added example

--- t.cpp
+++ t.cpp
@@ -22,7 +22,16 @@ int main()
   
   z1 += z2;
   
-  cout << z1 << endl;
+  cout << "Complex number example: " << z1 << endl;
+  cout << "The conjugate is: " << z1.conj() << endl;
+  cout << "The magnitude is: " << z1.abs() << endl;
+  cout << "The phase in Radians is: " << z1.arg() << endl;
+  cout << "The norm is: " << z1.norm() << endl;
+  cout << "The sqrt is: " << sqrt(z1) << endl;
+  cout << "The natural log is: " << log(z1) << endl;
+  cout << "The sin is: " << sin(z1) << endl;
+  cout << "The acosh is: " << acosh(z1) << endl;
+  cout << "Rounded to 2 decimal places: " << acosh(z1).rnd(2) << endl;
 
   return 0;
 }
