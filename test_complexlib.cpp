73b4c26a84f87d7603060cd164e0eabcf6d24593
Mike <mhutt@appternion.com>
2013-08-09 11:05:38 -0400

    adding test cases

--- test_complexlib.cpp
+++ test_complexlib.cpp
@@ -1,5 +1,5 @@
 #include "complex.h"
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
 Complex j = Complex(0.0,1.0);
 
@@ -30,3 +30,31 @@ TEST (CreateComplexNumber, SetInitValues2) {
 }
 
 
+TEST (Conjugate, CheckReal) {
+  Complex z = 1 + 2*j;
+  Complex c = z.conj();
+  EXPECT_EQ(1, c.re());
+}
+
+TEST (Conjugate, CheckImag) {
+  Complex z = 1 + 2*j;
+  Complex c = z.conj();
+  EXPECT_EQ(-2, c.im());
+}
+
+
+TEST (Magnitude, CheckAbs) {
+  Complex z = 3 + 4*j;
+  EXPECT_EQ(5,z.abs());
+}
+
+
+TEST (Phase, CheckArg) {
+  Complex z = 3 + 3*j;
+  EXPECT_GT(z.arg(), 0.78);
+}
+
+TEST (Norm, CheckNorm) {
+  Complex z = 3 + 3*j;
+  EXPECT_EQ(18, z.norm());
+}
